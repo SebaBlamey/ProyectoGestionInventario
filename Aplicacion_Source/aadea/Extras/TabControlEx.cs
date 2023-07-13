b9383a9681fe9a8c9cb2c8bcc5c6602964dd3029
SebaBlamey <sebastian.cortes04@alumnos.ucn.cl>
2023-07-12 22:13:26 -0400

    funcion para los table

---
+++ Aplicacion_Source/aadea/Extras/TabControlEx.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace aadea.Extras;
+
+public class TabControlEx : TabControl
+{
+    protected override void WndProc(ref Message m)
+    {
+        if (m.Msg == 0x1300 + 40)
+        {
+            RECT rc = (RECT)m.GetLParam(typeof(RECT));
+            rc.Left -= 6;
+            rc.Right += 6;
+            rc.Top -= 6;
+            rc.Bottom += 6;
+            Marshal.StructureToPtr(rc, m.LParam, true);
+        }
+        base.WndProc(ref m);
+    }
+}
+internal struct RECT { public int Left, Top, Right, Bottom; }
